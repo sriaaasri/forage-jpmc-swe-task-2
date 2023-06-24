ad77ffb44c4ec1e766d2a4bdbf0ce66685269a0f
sriaaasri <psatyachowdary2001@gmail.com>
2023-06-24 13:06:48 +0530

    ok

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -14,7 +14,7 @@ interface IProps {
  * Perspective library adds load to HTMLElement prototype.
  * This interface acts as a wrapper for Typescript compiler.
  */
-interface PerspectiveViewerElement {
+interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
 
@@ -27,12 +27,23 @@ class Graph extends Component<IProps, {}> {
   table: Table | undefined;
 
   render() {
-    return React.createElement('perspective-viewer');
+    return <>React.createElement('perspective-viewer');</>
   }
 
   componentDidMount() {
     // Get element to attach the table from the DOM.
-    const elem: PerspectiveViewerElement = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
+    elem.setAttribute('view','y_line');
+    elem.setAttribute('column-pivots','["stock"]');
+    elem.setAttribute('row-pivots','["timeStamp"]');
+    elem.setAttribute('columns','["top_ask_price"]');
+    elem.setAttribute('aggregates',`{
+      "stock":"distinct count",
+      "top_ask_price":"avg",
+      "top_bid_price":"avg",
+      "timpstamp":"distinct count"
+    }`)
+
 
     const schema = {
       stock: 'string',
@@ -59,6 +70,7 @@ class Graph extends Component<IProps, {}> {
       // avoid inserting duplicated entries into Perspective table again.
       this.table.update(this.props.data.map((el: any) => {
         // Format the data from ServerRespond to the schema
+
         return {
           stock: el.stock,
           top_ask_price: el.top_ask && el.top_ask.price || 0,
